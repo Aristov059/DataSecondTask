685fcb228c42353efb413265e544eebb80a09fab
sag1ttar <115164398+sag1ttar@users.noreply.github.com>
2023-11-17 22:15:27 +0500

    Initial commit

---
+++ app/src/main/java/com/example/datasecondtask/notes.java
@@ -0,0 +1,14 @@
+package com.example.datasecondtask;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class notes {
+
+    public String name;
+    public String text;
+    public String date;
+
+}
+
+
